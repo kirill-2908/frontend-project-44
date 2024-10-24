4944a67a4d03618ce96c9e893024381995f37cee
Kirill <olesyal360.iso@gmai.com>
2024-10-24 15:22:44 +0300

    bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+
+import { makeGreeting} from '/home/kirill19/project/frontend-project-44/src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+console.log(makeGreeting());
